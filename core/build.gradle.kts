5863e66b2ba91bfd078f4de33d904dc11cf290e5
Nico D'Cotta <nico.dc@outlook.com>
2021-05-22 12:00:47 +0200

    chore: add mocking lib

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
     testImplementation("io.kotest:kotest-runner-junit5:4.6.0")
+    testImplementation("io.mockk:mockk:1.11.0")
+
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
 }
