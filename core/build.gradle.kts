47d97a7c95f419e664229bb08ae32b675814f113
Nico D'Cotta <45274424+Cottand@users.noreply.github.com>
2021-05-23 13:45:41 +0100

    feat(commit): commitAsync wrapper (#4)
    * chore(CI): main.yml GH action
    
    * fix(gradle): remove .jar from gitignore and regen wrapper
    
    * fix(tests): rewrite generated test with kotest
    
    * chore(readme): add CI badge
    
    * chore(CI): use caching
    
    * chore: add mocking lib
    
    * fix(gradle) bump wrapper
    
    * chore(kotlin): enable library mode and warnings as errors
    
    * feat(consumer): write commit suspending extensions
    
    * refactor(commit): rename to Suspending

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,7 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.5.0"
+    kotlin("jvm") version "1.5.0"
     `java-library`
 }
 
@@ -10,9 +12,23 @@ repositories {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-    testImplementation("io.kotest:kotest-runner-junit5:4.6.0")
+    testImplementation("io.kotest:kotest-assertions-core:4.6.0")
     testImplementation("io.mockk:mockk:1.11.0")
+    api("org.apache.kafka:kafka_2.12:2.8.0")
 
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+}
+
+
+kotlin {
+    explicitApi()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.allWarningsAsErrors = true
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
---
+++ core/src/test/kotlin/org/kkafka/consumer/CommitKtTest.kt
@@ -0,0 +1,83 @@
+package org.kkafka.consumer
+
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.runBlocking
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.OffsetAndMetadata
+import org.apache.kafka.clients.consumer.OffsetCommitCallback
+import org.apache.kafka.common.TopicPartition
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.kkafka.commitSuspending
+
+internal class CommitKtTest {
+    private val someMap = mapOf(TopicPartition("cake", 1) to OffsetAndMetadata(0, "lies"))
+    private val consumer: KafkaConsumer<String, String> = mockk {
+        every { commitAsync(any()) } answers {
+            val callback = firstArg<OffsetCommitCallback>()
+            callback.onComplete(someMap, null)
+        }
+
+        every { commitAsync(any(), any()) } answers {
+            val callback = secondArg<OffsetCommitCallback>()
+            callback.onComplete(someMap, null)
+        }
+    }
+
+    @Test
+    fun `commit() calls commitAsync and continues`() {
+        runBlocking { consumer.commitSuspending() }
+
+        verify(exactly = 1) { consumer.commitAsync(any()) }
+    }
+
+    @Test
+    fun `commit() returns what commitAsync returns`() {
+        val result = runBlocking { consumer.commitSuspending() }
+
+        result shouldBe someMap
+    }
+
+    @Test
+    fun `commit() throws whatever is passed to the callback`() {
+        val someException = IllegalStateException("oopsie")
+
+        every { consumer.commitAsync(any()) } answers {
+            val callback = firstArg<OffsetCommitCallback>()
+            callback.onComplete(someMap, someException)
+        }
+
+        val exception = assertThrows<IllegalStateException> {
+            runBlocking { consumer.commitSuspending() }
+        }
+
+        exception.message shouldBe someException.message
+    }
+
+    @Test
+    fun `commit(offsets) calls commitAsync with passed params`() {
+        val someMap2 = mapOf(TopicPartition("cakes", 10) to OffsetAndMetadata(0, "more lies")) + someMap
+        runBlocking { consumer.commitSuspending(someMap2) }
+
+        verify(exactly = 1) { consumer.commitAsync(someMap2, any())  }
+    }
+
+    @Test
+    fun `commit(offsets) throws whatever is passed to the callback`() {
+        val someException = IllegalStateException("oopsie")
+
+        every { consumer.commitAsync(someMap, any()) } answers {
+            val callback = secondArg<OffsetCommitCallback>()
+            callback.onComplete(someMap, someException)
+        }
+
+        val exception = assertThrows<IllegalStateException> {
+            runBlocking { consumer.commitSuspending(someMap) }
+        }
+
+        exception.message shouldBe someException.message
+    }
+}
