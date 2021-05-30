0ae5aa62922e0b14aae97ab9e07c4843fa5baffa
Nico D'Cotta <nico.dc@outlook.com>
2021-05-30 12:40:47 +0200

    doc: set up dokka with html render and kafka links

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
+import org.jetbrains.dokka.gradle.DokkaPlugin
+import org.jetbrains.dokka.gradle.DokkaTaskPartial
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.KtlintPlugin
+import java.net.URL
 
 plugins {
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0" apply false
-    kotlin("jvm") version "1.5.0"
+    id("org.jetbrains.dokka") version "1.4.32"
+    kotlin("jvm") version "1.5.10"
     jacoco
 }
 
@@ -16,6 +20,7 @@ allprojects {
 subprojects {
     apply<KtlintPlugin>()
     apply<JacocoPlugin>()
+    apply<DokkaPlugin>()
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     kotlin {
@@ -36,6 +41,25 @@ subprojects {
                 html.isEnabled = false
             }
             dependsOn(test)
+        }
+
+        withType<DokkaTaskPartial>().configureEach {
+
+            dokkaSourceSets {
+                named("main") {
+                    moduleName.set("KKafka-${project.name}")
+                    includes.from("Module.md")
+                    sourceLink {
+                        localDirectory.set(file("src/main/kotlin"))
+                        remoteUrl.set(URL(
+                            "https://github.com/kkafkaorg/kkafka/tree/master/${project.name}/src/main/kotlin"
+                        ))
+                    }
+                    externalDocumentationLink(
+                        url = URL("https://javadoc.io/doc/org.apache.kafka/kafka-clients/2.8.0/")
+                    )
+                }
+            }
         }
     }
 }
