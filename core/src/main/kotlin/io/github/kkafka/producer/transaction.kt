6da18d8f7d2cea003e9c32b770ade95af9214db2
William Profit <williamprofit@hotmail.fr>
2021-06-19 11:59:54 +0200

    feat(transaction): add arbitrary block transaction

--- core/src/main/kotlin/io/github/kkafka/producer/transaction.kt
+++ core/src/main/kotlin/io/github/kkafka/producer/transaction.kt
@@ -14,11 +14,14 @@ import org.apache.kafka.clients.producer.KafkaProducer
  * [block] is the transaction code. Here you can consume from [consumer] and send with the producer.
  * At the end of the [block], if true is returned, then the transaction is committed, otherwise if false is returned,
  * it is aborted.
+ *
+ * This is useful for a consume-process-produce pattern, where we commit to the consumer only if we have successfully
+ * produced. This ensures exactly once processing.
  */
-public fun <PK, PV, CK, CV> KafkaProducer<PK, PV>.transaction(
-    consumer: KafkaConsumer<CK, CV>,
+public fun <K, V> KafkaProducer<K, V>.transaction(
+    consumer: KafkaConsumer<*, *>,
     consumerTopics: List<String>,
-    block: KafkaProducer<PK, PV>.() -> Boolean,
+    block: KafkaProducer<K, V>.() -> Boolean,
 ) {
     beginTransaction()
 
@@ -45,24 +48,32 @@ public fun <PK, PV, CK, CV> KafkaProducer<PK, PV>.transaction(
 
 /**
  * Consume and produce transactionally on a single topic.
+ * See [transaction].
  */
-public fun <PK, PV, CK, CV> KafkaProducer<PK, PV>.transaction(
-    consumer: KafkaConsumer<CK, CV>,
+public fun <K, V> KafkaProducer<K, V>.transaction(
+    consumer: KafkaConsumer<*, *>,
     consumerTopic: String,
-    block: KafkaProducer<PK, PV>.() -> Boolean,
+    block: KafkaProducer<K, V>.() -> Boolean,
 ) {
     transaction(consumer, listOf(consumerTopic), block)
 }
 
-public fun main() {
-    val producer = KafkaProducer<String, Int>(mapOf())
-    val consumer = KafkaConsumer<String, String>(mapOf())
-
-    consumer.subscribe(listOf("my-topic"))
-
-    producer.transaction(consumer, "my-topic") {
-        // Consume & produce
-
-        true
+/**
+ * Produce transactionally with an arbitrary block of code.
+ * If an exception occurs during execution of [block], the transaction is aborted, otherwise it is committed.
+ */
+public fun <K, V, T> KafkaProducer<K, V>.transaction(block: KafkaProducer<K, V>.() -> T) : T {
+    beginTransaction()
+    return try {
+        val res = block(this)
+        try {
+            commitTransaction()
+            res
+        } catch (onCommit: Exception) {
+            throw onCommit
+        }
+    } catch (e: Exception) {
+        abortTransaction()
+        throw e
     }
 }
